2bbb0b02228159e6fcdf5812bb9551c5136b961d
Nathan Boyd <nboyd@salesforce.com>
2017-01-31 17:30:09 -0500

    moving to repo focused on emacs

--- snippets/greaseMonkey/deployToBrowser.cmd
+++
@@ -1,4 +0,0 @@
-robocopy .\scripts %appdata%\Roaming\Mozilla\Firefox\Profiles\xuigwht8.default\gm_scripts /s
-
-timeout/t -1 
-echo on
--- snippets/greaseMonkey/scripts/jira/jiraInsertDescription.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        	jiraInsertDescription
-// @namespace   	jira
-// @version     	1
-// @grant       	none
-// ==/UserScript==
-
-console.log('foo');
